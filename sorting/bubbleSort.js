8bed56c8b914f06cf78e7b4b20f87e840c3eee76
Dilshad <dilshad_fw11_177@masai.school>
2022-03-20 17:13:50 +0530

    bubble and selection sort algorithm

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,9 +1,20 @@
 let arr = [1,3,-3,5,5,-2,0];
-// ascending  & desceding order
+// // ascending  & desceding order
 
-for(let i=0; i<arr.length-1; i++){
-    for(let j=0; j<arr.length-i; j++){
-        if(arr[j] > arr[j+1]){
+// for(let i=0; i<arr.length-1; i++){
+//     for(let j=0; j<arr.length-i; j++){
+//         if(arr[j] > arr[j+1]){
+//             let temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp;
+//         }
+//     }
+// }
+// console.log(arr)
+
+for(let i=0; i<arr.length - 1; i++ ){
+    for(let j=0; j<arr.length - i - 1; j++){
+        if(arr[j] < arr[j+1]){
             let temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,13 +1,26 @@
 let arr = [1,0,-5,-5, -3,-1,0,0,1];
 // ascending && descending order;
 
+// for(let i=0; i<arr.length; i++){
+//     let min = i;
+//     for(let j=i; j<arr.length; j++){
+//         if(arr[min] < arr[j]){
+//             min = j;
+//         }
+//     }
+//     [arr[i], arr[min]] = [arr[min], arr[i]];
+// }
+// console.log(arr)
+
 for(let i=0; i<arr.length; i++){
     let min = i;
     for(let j=i; j<arr.length; j++){
-        if(arr[min] < arr[j]){
+        if(arr[min] > arr[j]){
             min = j;
         }
     }
-    [arr[i], arr[min]] = [arr[min], arr[i]];
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
 }
 console.log(arr)

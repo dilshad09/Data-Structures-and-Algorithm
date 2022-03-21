9135f38769c97ddcba1845bb61d05374e374bf3e
Dilshad <dilshad_fw11_177@masai.school>
2022-03-21 23:19:09 +0530

    swap numbers with bit manipualtion

--- bitManipulation/swap.js
+++ bitManipulation/swap.js
@@ -5,3 +5,11 @@ a = a ^b;
 b = b ^ a ;
 a = a ^ b;
 console.log(a, b)
+
+// let name1 = "dilshad";
+// let name2 = "ahmad"
+// console.log(name1, name2)
+// name1 = name1^name2;
+// name2 = name2^name1;
+// name1 = name1^name2;
+// console.log(name1, name2)

7d17eddcaf8bd95ea26cbb9bbe659e3ad6e3ba80
Dilshad <dilshad_fw11_177@masai.school>
2022-03-21 23:50:45 +0530

    even odd with bit manipulation

---
+++ bitManipulation/evenOdd.js
@@ -0,0 +1,7 @@
+let num = 14;
+
+if(num & 1 === 1){
+    console.log("Odd")
+}else{
+    console.log("Even")
+}
---
+++ bitManipulation/xorOfN.js
@@ -0,0 +1,6 @@
+let n = 9;
+let xor =  0;
+for(let i=1; i<=n; i++){
+    xor ^= i;
+}
+console.log(xor)

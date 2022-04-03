c4cefa1b9c254cccc9c0f38f786d75ed5ebc913f
Dilshad <dilshad_fw11_177@masai.school>
2022-04-03 16:00:56 +0530

    max/min of an array using pure recursion without any extra variable solved

--- Recursion_Backtracking/maxOfArray.js
+++ Recursion_Backtracking/maxOfArray.js
@@ -1,12 +1,28 @@
-let arr = [4,5,1,-1,1,0] 
-let max = Infinity;
-console.log(maxOfArray(0, arr))
+// let arr = [4,5,1,-1,1,0]; 
+// let max = Infinity;
+// console.log(maxOfArray(0, arr))
 
-function maxOfArray(i, arr){
-     if(arr[i] === undefined) return;
+// function maxOfArray(i, arr){
+//      if(arr[i] === undefined) return;
 
      
-     if(arr[i] < max) max = arr[i];
-      maxOfArray(i+1, arr)
-      return max;
+//      if(arr[i] < max) max = arr[i];
+//       maxOfArray(i+1, arr)
+//       return max;
+// }
+
+let arr = [2,5,0,1,-15,2]
+function maxOfArray(i, arr){
+
+      if(i === arr.length - 1){
+          return arr[i]
+      }
+    let maxInSmallerArr = maxOfArray(i+1, arr)
+    if(maxInSmallerArr < arr[i]){
+        return maxInSmallerArr;
+    }else{
+        return arr[i]
+    }
 }
+
+console.log(maxOfArray(0, arr))

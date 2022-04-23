114284d9a3da21f52865c56381760e4247649f62
Dilshad <dilshad_fw11_177@masai.school>
2022-04-23 10:24:12 +0530

    insertion sorting solved

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -37,21 +37,39 @@
 // }
 // console.log(arr)
 
-let arr = [1,0,5,-1,3,-4];
+
 
 // insertion sort- time complexity worst(if array is sortedin descending order and you are sorting in ascending order) O(n^2)
 // best case (if array is sorted in ascending order and you are sorting in  ascending order)
 
 // insertion sort - sorted sublist and unsorted sublist
-for(let i=1; i<arr.length; i++){
-    let temp = arr[i];
-    let j = i - 1;
+// for(let i=1; i<arr.length; i++){
+//     let temp = arr[i];
+//     let j = i - 1;
 
     
-    while( j>=0 &&  arr[j] < temp){
-        arr[j+1] = arr[j];
+//     while( j>=0 &&  arr[j] < temp){
+//         arr[j+1] = arr[j];
+//         j--;
+//     }
+//     arr[j + 1] = temp;
+// }
+// console.log(arr)
+
+
+let arr = [1,0,0,1,0,0];
+let n = arr.length;
+
+for(let i=0; i<n; i++){
+    let j = i+1;
+
+    while(j>=0){
+        if(arr[j] > arr[j-1]){
+            [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
+        }else{
+            break;
+        }
         j--;
     }
-    arr[j + 1] = temp;
 }
 console.log(arr)

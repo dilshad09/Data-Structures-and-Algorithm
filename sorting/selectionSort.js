6ec1cc82469e75718778a84fafcbef9548ebd51c
Dilshad <dilshad_fw11_177@masai.school>
2022-04-23 09:54:31 +0530

    selection sort revised

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -12,15 +12,29 @@ let arr = [1,0,-5,-5, -3,-1,0,0,1];
 // }
 // console.log(arr)
 
-for(let i=0; i<arr.length; i++){
+// for(let i=0; i<arr.length; i++){
+//     let min = i;
+//     for(let j=i; j<arr.length; j++){
+//         if(arr[min] > arr[j]){
+//             min = j;
+//         }
+//     }
+//     let temp = arr[i];
+//     arr[i] = arr[min];
+//     arr[min] = temp;
+// }
+// console.log(arr)
+
+
+let n = arr.length;
+
+for(let i=0; i<n; i++){
     let min = i;
-    for(let j=i; j<arr.length; j++){
-        if(arr[min] > arr[j]){
+    for(let j=i; j<n; j++){
+        if(arr[min] < arr[j]){
             min = j;
         }
     }
-    let temp = arr[i];
-    arr[i] = arr[min];
-    arr[min] = temp;
+    [arr[i], arr[min]] = [arr[min], arr[i]];
 }
 console.log(arr)
